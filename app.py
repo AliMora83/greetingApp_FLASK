220d237ce6c923611cac184469a1a3f5fc8aadc5
Daurius Swarts <ali@openmindi.co.za>
2022-11-27 16:51:11 +0200

    update

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "5#y2L_namka_09"
 
+students = []
+
 
 @app.route("/")
 def index():
-    flash("Hello 🏆 What's your name?")
     return render_template("index.html")
 
 
-@app.route("/greet", methods=['POST', 'GET'])
-def greeter():
-    name = request.form["name_input"]
-    flash("🏆 Hi " + str(name) + ", great to see you! 🥳")
-    if not name:
-        return "failure"
+@app.route("/registrants")
+def registrants():
+    return render_template("registered.html", students=students)
 
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    name = request.form("name")
+    dorm = request.form("dorm")
+    if not name or not dorm:
+        return render_template("failure.html")
+    students.append(f"{name} from {dorm}")
     return render_template("success.html")
