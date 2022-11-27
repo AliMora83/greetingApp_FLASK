62feb8371bfd8c2b2aeb039a5217e8343067651f
Daurius Swarts <ali@openmindi.co.za>
2022-11-27 21:21:05 +0200

    Update

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, flash
+import csv
 
 app = Flask(__name__)
 app.secret_key = "5#y2L_namka_09"
 
 students = []
+name = request.form.get("name")
+city = request.form.get("city")
 
 
 @app.route("/")
@@ -11,16 +14,24 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/registrants")
-def registrants():
-    return render_template("registered.html", students=students)
-
-
-@app.route("/register", methods=['POST', 'GET'])
+@app.route("/register", methods=['POST'])
 def register():
-    name = request.form("name")
-    dorm = request.form("dorm")
-    if not name or not dorm:
+
+    if not name or not city:
         return render_template("failure.html")
-    students.append(f"{name} from {dorm}")
+    students.append(f"{name} from {city}")
+    file = open("registered.csv", "a")
+    writer = csv.writer(file)
+    writer.writerow((request.form.get("name")), request.form.get("city"))
+    file.close()
+
     return render_template("success.html")
+
+
+@app.route("/registered")
+def registered():
+    with open("registered.csv", "r") as file:
+        reader = csv.reader(file)
+        students = list(reader)
+
+    return render_template("registered.html", students=students)
